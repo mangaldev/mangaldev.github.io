d01e439fd160039adb77d2f9de729b7473c28ffa
mangaldev <mdev@gracenote.com>
2015-07-30 00:55:13 -0700

    chagned

---
+++ UrbanLadder/src/BallBoxFit.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by mdev on 5/7/15.
+ */
+public class BallBoxFit {
+    static int[] balls = {10,2,3,4,6,11};
+    static int[] boxes = {2,3,4,6,10,11};
+    Map<Integer,Integer> resultMap = new HashMap<>();
+    public static void main(String[] args) {
+
+    }
+
+    public static void fitBalls(){
+
+        for (int i = 0; i < balls.length; i++) {
+
+        }
+    }
+}
